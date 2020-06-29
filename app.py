d2ebde11a1f85ef66008ab8f27c0bb9dd9460394
felixyh <felix_yanghui@sina.cn>
2020-06-29 20:15:03 +0800

    first submit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request, render_template
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    user_agent = request.headers.get('User-Agent')
+    urlmap = app.url_map
+    return "Hello, Flask! " + "<p>Your browser is %s</p>" % user_agent + "<p>this is URL map %s </p>" % urlmap
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
